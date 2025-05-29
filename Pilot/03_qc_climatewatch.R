a50991bcd41f0d778c63e39f375a47b0c34f4ede
Alexis Athens <lexisathens1@gmail.com>
2025-05-29 11:21:13 -0400

    Create method to map members_id to dimensions

--- Pilot/03_qc_climatewatch.R
+++ Pilot/03_qc_climatewatch.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(magrittr)
 library(readxl)
+library(httr2)
+library(jsonlite)
 library(glue)
 library(lubridate)
 
@@ -26,8 +28,17 @@ latest_file <- tibble(file = file_list) %>%
   pull(file)
 
 # Read in csv
-data <- read_csv(latest_file)
-# will need to clean this further, but pause on it for now until the final format (compared to CEPALSTAT UI) is understood
+data <- read_csv(latest_file, col_types = cols(members_id = col_character()))
+
+# Split out members_id into separate columns
+data %<>%
+  mutate(members_vec = str_split(members_id, ",")) %>%
+  unnest_wider(members_vec, names_sep = "_") %>%
+  rename_with(~ paste0("dim_", letters[seq_along(.)]), starts_with("members_vec_")) # give generic dimension name for now
+
+# Keep relevant columns for data comparison
+data %<>% 
+  select(record_id, value, starts_with("dim_"))
 
 # ---- Download current public version of indicator ----
 
@@ -43,6 +54,53 @@ result <- request(url) %>%
 pub <- result$body$data %>%
   as_tibble()
 
+# Force dim_* columns to characters
+pub %<>% 
+  mutate(across(starts_with("dim_"), as.character))
+
+# Keep relevant columns for data comparison
+pub %<>% 
+  select(value, starts_with("dim_"))
+
+# ---- Distinguish how columns match ----
+
+set.seed(123)  # for reproducibility
+
+# Sample 3 values from each column of data_dims
+sampled_values <- data %>%
+  select(starts_with("dim_")) %>%
+  reframe(across(everything(), ~ sample(unique(.x), size = 3))) %>%
+  pivot_longer(everything(), names_to = "dim_col", values_to = "sampled_val")
+
+# Get distinct values from each dim_* column in pub
+pub_long <- pub %>%
+  select(starts_with("dim_")) %>%
+  pivot_longer(everything(), names_to = "dim_col", values_to = "pub_val") %>%
+  distinct()
+
+# Get mapping from sampled_values to pub cols
+dim_map <- pub_long %>% 
+  left_join(sampled_values, by = c("pub_val" = "sampled_val"), suffix = c("", ".match")) %>% 
+  filter(!is.na(dim_col.match)) %>% 
+  distinct(dim_col, dim_col.match)
+
+# Throw an error if more matches than dimensions
+if(nrow(dim_map) != ncol(data %>% select(starts_with("dim_")))) {
+  stop(glue::glue("Error: dim_map has {nrow(dim_map)} rows, but {ncol(data %>% select(starts_with('dim_')))} dimension columns found in data."))
+}
+
+# Rename columns of data with correct dimension IDs
+data %<>%
+  rename(!!!setNames(dim_map$dim_col.match, dim_map$dim_col))
+
+# ---- Create comparison data frame ----
+
+data %>% 
+  full_join(pub, by = dim_map$dim_col)
+
+dim_map$dim_col
+
+data
 pub
 
 
