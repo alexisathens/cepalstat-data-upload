e51f3a28e38b80be8656866f1f618d9c567f163a
Alexis Athens <lexisathens1@gmail.com>
2025-06-02 10:44:23 -0400

    Add flag for whether ECLAC country

--- Pilot/build-iso-table.R
+++ Pilot/build-iso-table.R
@@ -14,15 +14,28 @@ iso <- mp_iso %>%
 iso %<>% 
   rename(iso2 = alpha2, iso3 = alpha3)
 
+## Read in my iso code file that only has ECLAC countries
+my_iso <- read_excel("C:/Users/aathens/OneDrive - United Nations/Documentos/LAC_countries.xlsx")
+
+my_iso %<>% 
+  select(iso3, spanish)
+
+iso %<>% 
+  left_join(my_iso, by = "iso3") %>% 
+  mutate(ECLAC = ifelse(!is.na(spanish), TRUE, FALSE)) %>% 
+  select(-spanish)
+
 # create entry for world
-world <- tibble(name = "World", iso2 = NA_character_, iso3 = "WLD", numeric = NA_real_)
+world <- tibble(name = "World", iso2 = NA_character_, iso3 = "WLD", numeric = NA_real_, ECLAC = FALSE)
 
 iso %<>% bind_rows(world)
 
 # Create another iso with full WORLD spelled out (also common)
 iso %<>% 
   mutate(iso = ifelse(name == "World", "WORLD", iso3)) %>% 
-  select(name, iso2, iso3, iso, numeric)
+  select(name, iso2, iso3, iso, numeric, ECLAC)
+
+
 
 # Export spreadsheet
 write_csv(iso, "C:/Users/aathens/OneDrive - United Nations/Documentos/CEPALSTAT Data Process/cepalstat-data-upload/Data/iso_codes.csv")
