0d98cf6e4ee2d608c05140d22ec06b74554ef4a7
Alexis Athens <lexisathens1@gmail.com>
2025-06-04 17:17:57 -0400

    Finish process for simplest indicator, 1754

--- Pilot/02_preprocess_olade.R
+++ Pilot/02_preprocess_olade.R
@@ -64,6 +64,9 @@ grupo1 %<>%
 grupo1 %<>% 
   filter(Country %in% iso$spanish_short)
 
+
+### grupo-specific cleaning:
+
 # Make data long
 grupo1 %<>% 
   pivot_longer(cols = -c(Country, Years),
@@ -91,8 +94,74 @@ i2487 %>%
   filter(!Type %in% d2487$name) %>% 
   distinct(Type)
 
-# Remove total rows
-i2487 %>% 
-  filter(!Type %in% c("Total primarias", "Total secundarias", "Total"))
 
-# Remove total rows
+
+
+
+#### GRUPO 04: unidad - energetico - unidad - consumo energetico - electricidad ####
+
+## Clean into standard flat data format
+
+grupo4 <- read_excel(paste0(input_path, "olade_grupo4.xlsx"), col_names = FALSE)
+
+# Extract header and unit rows for each table in spreadsheet
+header_row <- grupo4[3,]
+unit_row <- grupo4[4,]
+
+# Remove rows that match these patterns
+grupo4 %<>% 
+  anti_join(header_row) %>% 
+  anti_join(unit_row)
+
+# Format header row
+header_row %<>% unlist(use.names = FALSE) %>% str_trim()
+header_row[1] <- "Country"
+colnames(grupo4) <- header_row
+
+# Create year field
+grupo4 %<>% 
+  mutate(Years = str_extract(Country, "\\b\\d{4}$")) %>% 
+  fill(Years, .direction = "down") %>% 
+  select(Country, Years, everything())
+
+# Remove year header and first row
+grupo4 %<>% 
+  filter(!str_detect(Country, "\\b\\d{4}$")) %>% 
+  filter(Country != "Series de oferta y demanda")
+
+# Filter countries
+iso <- read_csv("../Data/iso_codes.csv")
+
+iso %<>% 
+  filter(!is.na(spanish_short)) %>% 
+  select(name, spanish_short)
+
+# Check what countries don't match over that should
+grupo4 %>% 
+  filter(!Country %in% iso$spanish_short) %>% 
+  distinct(Country)
+
+# Force OLADE names to match CEPALSTAT names
+grupo4 %<>% 
+  mutate(Country = case_when(
+    Country == "Grenada" ~ "Granada",
+    Country == "Trinidad & Tobago" ~ "Trinidad y Tabago",
+    TRUE ~ Country
+  ))
+
+# Filter on CEPALSTAT countries only
+grupo4 %<>% 
+  filter(Country %in% iso$spanish_short)
+
+
+### grupo-specific cleaning:
+
+grupo4 %<>% rename(value = Electricidad)
+
+
+
+# ---- 1754: consumption of electric power ----
+
+i1754 <- grupo4
+
+i1754
