bace364785c62a0937a5b4e6f415855096f59867
Alexis Athens <lexisathens1@gmail.com>
2025-06-20 13:02:04 -0400

    Refactor with cursor, default to binding Spanish names

---
+++ utils.R
@@ -0,0 +1,102 @@
+# Helper: Fetch and parse JSON from CEPALSTAT API
+fetch_cepalstat_json <- function(url) {
+  request(url) %>%
+    req_perform() %>%
+    resp_body_string() %>%
+    fromJSON(flatten = TRUE)
+}
+
+# # Helper: Join English and Spanish names, add 'name_es' column
+# add_spanish_names <- function(df_en, df_es) {
+#   df_en %>%
+#     left_join(df_es %>% select(id, name_es = name), by = "id")
+# }
+
+# Get indicator dimensions with English and Spanish names
+get_indicator_dimensions <- function(indicator_id) {
+  # English
+  url_en <- glue("https://api-cepalstat.cepal.org/cepalstat/api/v1/indicator/{indicator_id}/dimensions?lang=en&format=json&in=1&path=0")
+  result_en <- fetch_cepalstat_json(url_en)
+  dims_en <- result_en %>%
+    pluck("body", "dimensions") %>%
+    as_tibble() %>%
+    select(name, id)
+  
+  # Spanish
+  url_es <- glue("https://api-cepalstat.cepal.org/cepalstat/api/v1/indicator/{indicator_id}/dimensions?lang=es&format=json&in=1&path=0")
+  result_es <- fetch_cepalstat_json(url_es)
+  dims_es <- result_es %>%
+    pluck("body", "dimensions") %>%
+    as_tibble() %>%
+    select(name, id)
+  
+  # Join Spanish names as 'name_es'
+  dims <- dims_en %>%
+    left_join(dims_es %>% select(id, name_es = name), by = "id")
+  
+  return(dims)
+}
+
+# Get full dimension table (members) with English and Spanish names
+get_full_dimension_table <- function(dimension_id) {
+  # English
+  url_en <- glue("https://api-cepalstat.cepal.org/cepalstat/api/v1/dimensions/{dimension_id}?lang=en")
+  result_en <- fetch_cepalstat_json(url_en)
+  dim_info_en <- result_en %>%
+    pluck("body", "dimensions") %>%
+    as_tibble()
+  members_en <- dim_info_en$members[[1]] %>% as_tibble()
+  members_en <- members_en %>%
+    mutate(dim_id = dim_info_en$id[1],
+           dim_name = dim_info_en$name[1])
+  
+  # Spanish
+  url_es <- glue("https://api-cepalstat.cepal.org/cepalstat/api/v1/dimensions/{dimension_id}?lang=es")
+  result_es <- fetch_cepalstat_json(url_es)
+  dim_info_es <- result_es %>%
+    pluck("body", "dimensions") %>%
+    as_tibble()
+  members_es <- dim_info_es$members[[1]] %>% as_tibble()
+  members_es <- members_es %>%
+    mutate(dim_id = dim_info_es$id[1],
+           dim_name = dim_info_es$name[1])
+  
+  # Join Spanish names as '*_es'
+  members <- members_en %>%
+    left_join(members_es %>% select(id, name_es = name, dim_name_es = dim_name), by = "id")
+  
+  return(members)
+}
+
+# Get indicator-specific dimension members for an indicator, with English and Spanish names
+get_ind_dimension_table <- function(indicator_id, dimension_id) {
+  # English
+  url_en <- glue("https://api-cepalstat.cepal.org/cepalstat/api/v1/indicator/{indicator_id}/dimensions?lang=en&format=json&in=1&path=0")
+  result_en <- fetch_cepalstat_json(url_en)
+  indicator_info_en <- result_en %>%
+    pluck("body", "dimensions") %>%
+    as_tibble() %>%
+    filter(id == dimension_id)
+  members_en <- indicator_info_en$members[[1]] %>% as_tibble()
+  members_en <- members_en %>%
+    mutate(dim_id = indicator_info_en$id[1],
+           dim_name = indicator_info_en$name[1])
+  
+  # Spanish
+  url_es <- glue("https://api-cepalstat.cepal.org/cepalstat/api/v1/indicator/{indicator_id}/dimensions?lang=es&format=json&in=1&path=0")
+  result_es <- fetch_cepalstat_json(url_es)
+  indicator_info_es <- result_es %>%
+    pluck("body", "dimensions") %>%
+    as_tibble() %>%
+    filter(id == dimension_id)
+  members_es <- indicator_info_es$members[[1]] %>% as_tibble()
+  members_es <- members_es %>%
+    mutate(dim_id = indicator_info_es$id[1],
+           dim_name = indicator_info_es$name[1])
+  
+  # Join Spanish names as '*_es'
+  members <- members_en %>%
+    left_join(members_es %>% select(id, name_es = name, dim_name_es = dim_name), by = "id")
+  
+  return(members)
+}
