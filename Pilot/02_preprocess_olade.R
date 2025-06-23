cbd69a8c16b6d2d17e699fe4ff71923c53d8eda4
Alexis Athens <lexisathens1@gmail.com>
2025-06-23 11:30:12 -0400

    Add cleaning for 2486 with dim type

--- Pilot/02_preprocess_olade.R
+++ Pilot/02_preprocess_olade.R
@@ -72,6 +72,9 @@ grupo1 %<>%
   pivot_longer(cols = -c(Country, Years),
                names_to = "Type")
 
+grupo1 %<>% 
+  mutate(value = as.numeric(value))
+
 # Store types for reference
 energy_types <- unique(grupo1$Type)
 
@@ -81,17 +84,94 @@ energy_types <- unique(grupo1$Type)
 id <- 2486
 i2486 <- grupo1
 
+
+## function this: 
 get_indicator_dimensions(2486)
 # 44959 - renewable and non-renewable primary energy type
 
 ## compare dimensions associated with id, versus full dimensions, versus dimensions in data
-get_full_dimension_table(44959)
+# summarize data to dimension level
+d44959 <- i2486 %>% 
+  group_by(Type) %>% 
+  mutate(value = as.numeric(value)) %>% 
+  summarize(value = sum(value, na.rm = TRUE)) %>% 
+  ungroup()
 
-get_ind_dimension_table(2486, 44959)
+d44959 %<>% 
+  mutate(DATA = "Y")
+
+# get full dimension table names
+f44959 <- get_full_dimension_table(44959)
+
+f44959 %<>% 
+  select(name_es, name, id) %>% 
+  mutate(FULL = "Y")
+
+# get current indicator dimension table
+c44959 <- get_ind_dimension_table(2486, 44959)
+
+c44959 %<>% 
+  select(id) %>% 
+  mutate(CURR = "Y")
+
+## join into combined table to cross-check dims
+comb44959 <- d44959 %>% 
+  full_join(f44959, by = c("Type" = "name_es")) %>% 
+  full_join(c44959, by = c("id"))
+
+comb44959
 
 
+### make manual adjustments to data labels
+i2486 %<>% 
+  mutate(Type = case_when(
+    Type == "Bagazo de caña" ~ "Caña de azúcar y derivados",
+    TRUE ~ Type
+  ))
 
-d2486
+
+### filter only on labels in CEPALSTAT dims
+# (here keep only primary data sources)
+cs44959 <- comb44959 %>% 
+  filter(FULL == "Y")
+
+i2486 %<>% 
+  filter(Type %in% cs44959$Type)
+
+
+### create summary rows
+
+clean_renew <- i2486 %>% 
+  filter(Type %in% c("Hidroenergía", "Geotermia", "Otras primarias"))
+
+nonclean_renew <- i2486 %>% 
+  filter(Type %in% c("Leña", "Caña de azúcar y derivados"))
+
+nonrenew <- i2486 %>% 
+  filter(Type %in% c("Petróleo", "Gas natural", "Carbón mineral", "Nuclear"))
+
+# summarize
+clean_renew %<>% group_by(Country, Years) %>% summarize(value = sum(value, na.rm = TRUE))
+nonclean_renew %<>% group_by(Country, Years) %>% summarize(value = sum(value, na.rm = TRUE))
+nonrenew %<>% group_by(Country, Years) %>% summarize(value = sum(value, na.rm = TRUE))
+
+# label
+clean_renew %<>% mutate(Type = "Energía primaria renovable limpia") %>% select(Country, Years, Type, value)
+nonclean_renew %<>% mutate(Type = "Energía primaria renovable no limpia") %>% select(Country, Years, Type, value)
+nonrenew %<>% mutate(Type = "Energía primaria no renovable") %>% select(Country, Years, Type, value)
+
+### join summary rows to data table
+
+i2486 %<>% 
+  bind_rows(clean_renew) %>% 
+  bind_rows(nonclean_renew) %>% 
+  bind_rows(nonrenew)
+
+i2486 %<>% 
+  arrange(Country, Years, Type) %>% 
+  filter(!is.na(value))
+
+i2486
 
 
 # ---- 2487: primary & secondary energy supply ----
