fa2b1f18df112cd396e77afc3606eeeb706078c9
Alexis Athens <lexisathens1@gmail.com>
2025-06-23 11:49:08 -0400

    Initialize

---
+++ Pilot/02_process_olade.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(magrittr)
+library(readxl)
+library(httr2)
+library(jsonlite)
+library(glue)
+library(writexl)
+
+# This script does the full cleaning and standardizing of OLADE indicators
+# (This is an attempted streamlining of 02_preprocess_olade and 03_process)
+
