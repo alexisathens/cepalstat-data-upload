a945f92a52ff4cd9d819d30ffeae298cb222a063
Alexis Athens <lexisathens1@gmail.com>
2025-07-03 15:35:25 -0400

    Swap assert_no_na function to be more informative and col-based

--- Scripts/utils.R
+++ Scripts/utils.R
@@ -5,11 +5,20 @@ library(jsonlite)
 library(glue)
 
 # Stop code if there are any NA fields that didn't match
-assert_no_na <- function(data) {
-  na_count <- sum(is.na(data))
+# Sample usage: assert_no_na_cols(i2486f, cols = ends_with("_id"))
+assert_no_na_cols <- function(data, cols = everything(), data_name = deparse(substitute(data))) {
+  na_summary <- data %>%
+    select({{ cols }}) %>%
+    summarise(across(everything(), ~ sum(is.na(.)))) %>%
+    pivot_longer(everything(), names_to = "column", values_to = "na_count") %>%
+    filter(na_count > 0)
   
-  if (na_count > 0) {
-    stop(glue::glue("Error: the data contains {na_count} missing value(s)."))
+  if (nrow(na_summary) > 0) {
+    msg <- paste0(
+      "❌ NA values found in ", data_name, ":\n",
+      paste0(" - ", na_summary$column, ": ", na_summary$na_count, " NA(s)", collapse = "\n")
+    )
+    stop(msg)
   }
   
   invisible(TRUE)
@@ -211,82 +220,82 @@ format_for_wasabi <- function(data, indicator_id){
 }
 
 # Take long df with value and dimensions only and create map to CEPALSTAT dimensions and fields
-get_data_dim_map <- function(dims_vector, data) {
-  
-  iso <- read_xlsx(here("Data/iso_codes.xlsx"))
-  
-  # Take a sample of the values in the long df
-  set.seed(123)
-  sampled_values <- data %>%
-    select(!value) %>%
-    reframe(across(everything(), ~ sample(unique(.x), size = 50, replace = TRUE)))
-  
-  # check sampled_values = same length as dims_vector
-  
-  # Cycle through each sampled column value (and find match in dim table)
-  match_results <- NULL
-  for(i in 1:ncol(sampled_values)) {
-    
-    this_sampled_col <- sampled_values[[i]]
-    
-    # Cycle through each dimension table
-    for(j in 1:length(dims_vector)){
-      
-      # Get table for this dimension
-      this_dim_id <- dims_vector[j]
-      this_dim_table <- get_full_dimension_table(this_dim_id)
-      
-      # Calculate match scores
-      result <- tibble(
-        column = names(sampled_values[,i]),
-        dim_id = this_dim_id,
-        iso = sum(this_sampled_col %in% iso$name, na.rm = T),
-        cs_en = sum(this_sampled_col %in% this_dim_table$name, na.rm = T),
-        cs_es = sum(this_sampled_col %in% this_dim_table$name_es, na.rm = T))
-      
-      match_results %<>% bind_rows(result)
-    }
-  }
-  
-  # Make sense of match_results
-  # match_results
-  
-  # Only keep dimension with most matches
-  col_map <- match_results %>% 
-    mutate(sum = iso + cs_en + cs_es) %>% 
-    group_by(column) %>% 
-    arrange(desc(sum)) %>% 
-    slice(1) %>% 
-    ungroup() %>% 
-    select(-sum)
-  
-  col_map %<>%
-    rowwise() %>%
-    mutate(
-      field = case_when(
-        iso >= cs_en & iso >= cs_es ~ "iso",
-        cs_en >= cs_es ~ "name",
-        TRUE ~ "name_es")) %>% 
-    ungroup() %>% 
-    select(data_column = column, dim_id, dim_field = field)
-  
-  return(col_map)
-}
+# get_data_dim_map <- function(dims_vector, data) {
+#   
+#   iso <- read_xlsx(here("Data/iso_codes.xlsx"))
+#   
+#   # Take a sample of the values in the long df
+#   set.seed(123)
+#   sampled_values <- data %>%
+#     select(!value) %>%
+#     reframe(across(everything(), ~ sample(unique(.x), size = 50, replace = TRUE)))
+#   
+#   # check sampled_values = same length as dims_vector
+#   
+#   # Cycle through each sampled column value (and find match in dim table)
+#   match_results <- NULL
+#   for(i in 1:ncol(sampled_values)) {
+#     
+#     this_sampled_col <- sampled_values[[i]]
+#     
+#     # Cycle through each dimension table
+#     for(j in 1:length(dims_vector)){
+#       
+#       # Get table for this dimension
+#       this_dim_id <- dims_vector[j]
+#       this_dim_table <- get_full_dimension_table(this_dim_id)
+#       
+#       # Calculate match scores
+#       result <- tibble(
+#         column = names(sampled_values[,i]),
+#         dim_id = this_dim_id,
+#         iso = sum(this_sampled_col %in% iso$name, na.rm = T),
+#         cs_en = sum(this_sampled_col %in% this_dim_table$name, na.rm = T),
+#         cs_es = sum(this_sampled_col %in% this_dim_table$name_es, na.rm = T))
+#       
+#       match_results %<>% bind_rows(result)
+#     }
+#   }
+#   
+#   # Make sense of match_results
+#   # match_results
+#   
+#   # Only keep dimension with most matches
+#   col_map <- match_results %>% 
+#     mutate(sum = iso + cs_en + cs_es) %>% 
+#     group_by(column) %>% 
+#     arrange(desc(sum)) %>% 
+#     slice(1) %>% 
+#     ungroup() %>% 
+#     select(-sum)
+#   
+#   col_map %<>%
+#     rowwise() %>%
+#     mutate(
+#       field = case_when(
+#         iso >= cs_en & iso >= cs_es ~ "iso",
+#         cs_en >= cs_es ~ "name",
+#         TRUE ~ "name_es")) %>% 
+#     ungroup() %>% 
+#     select(data_column = column, dim_id, dim_field = field)
+#   
+#   return(col_map)
+# }
 
 # Take dimension map to long df with value and dimensions only and join them
-join_data_dim_members <- function(dim_map, data) {
+join_data_dim_members <- function(data, dim_config) {
   
-  for(i in 1:nrow(dim_map)) {
-    data_col <- dim_map$data_column[i]
-    dim_id <- dim_map$dim_id[i]
-    dim_field <- dim_map$dim_field[i]
+  for(i in 1:nrow(dim_config)) {
+    data_col <- dim_config$data_col[i]
+    dim_id <- dim_config$dim_id[i]
+    dim_field <- str_remove(dim_config$pub_col[i], "^\\d+_")
     
     if(dim_field == "iso"){
-      matching_dim_id <- dim_map$dim_id[i]
+      matching_dim_id <- dim_config$dim_id[i]
       matching_dim_col <- read_xlsx(here("Data/iso_codes.xlsx")) %>% select(id = cepalstat, match = name) %>% distinct(id, match)
       matching_dim_col %<>% rename(!!paste0("d", dim_id, "_id") := id)
     } else {
-      matching_dim_id <- dim_map$dim_id[i]
+      matching_dim_id <- dim_config$dim_id[i]
       matching_dim_table <- get_full_dimension_table(matching_dim_id)
       matching_dim_col <- matching_dim_table %>% select(id, match = !!sym(dim_field))
       matching_dim_col %<>% rename(!!paste0("d", dim_id, "_id") := id)
@@ -325,7 +334,7 @@ match_cepalstat_labels <- function(pub) {
       left_join(this_dim_table, by = setNames(this_pub_col, this_pub_col))
   }
   
-  assert_no_na(pub)
+  assert_no_na_cols(pub)
   
   return(pub)
 }
