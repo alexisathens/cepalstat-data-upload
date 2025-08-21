f860e56e4c2e07ef03619557fb0e747ea07fe23c
Alexis Athens <lexisathens1@gmail.com>
2025-08-21 17:05:22 -0400

    Add indicator 2531 cleaning

--- Scripts/0102_fao.R
+++ Scripts/0102_fao.R
@@ -389,3 +389,171 @@ comp
 
 # Export comp file!
 # write_xlsx(comp, here(glue("Data/Checks/comp_id{indicator_id}.xlsx")))
+
+
+### ---- Proportion of forest plantations of total forest (2531) ----
+
+# Indicator name: Proportion of forest plantations on the surface of total forest
+# General instructions: Forest plantations / total forest
+
+indicator_id <- 2531
+i2531 <- land
+
+### filter on applicable Types/Calculations *****
+
+i2531 %<>% 
+  filter(Calculation == "area") %>% 
+  filter(Type %in% c("Forest land", "Planted Forest")) %>% 
+  select(-Calculation)
+
+# remove regional totals, construct ECLAC total from sum of countries
+i2531 %<>% 
+  filter(!Country %in% c("South America", "Central America", "Caribbean"))
+
+### ********************************
+
+
+# Fill out dim config table using following info:
+get_indicator_dimensions(indicator_id)
+
+pub <- get_cepalstat_data(indicator_id)
+pub <- match_cepalstat_labels(pub)
+pub
+
+dim_config2531 <- tibble(
+  data_col = c("Country", "Years"),
+  dim_id = c("208", "29117"),
+  pub_col = c("208_name", "29117_name")
+)
+
+# ---- harmonize labels and filter to final set ----
+
+### make manual adjustments to data labels *****
+
+# Calculate proportion of natural forest
+i2531 %<>% 
+  mutate(Type = ifelse(Type == "Forest land", "total", "planted")) %>% 
+  pivot_wider(names_from = Type, values_from = value) %>% 
+  mutate(value = round(planted / total * 100, 1))
+
+# Remove countries without more detailed forest data
+i2531 %<>% 
+  filter(!is.na(value))
+
+# Check country length for incomplete series
+# i2531 %>%
+#   group_by(Country) %>%
+#   count() %>%
+#   arrange(n)
+
+# Remove countries with incomplete entries to not affect aggregate LatAm calculation
+i2531 %<>% 
+  filter(!Country %in% c("Curaçao", "Sint Maarten (Dutch part)"))
+
+# Create ECLAC regional total
+eclac_totals <- i2531 %>% 
+  group_by(Years) %>% 
+  summarize(total = sum(total, na.rm = T),
+            planted = sum(planted, na.rm = T), .groups = "drop") %>% 
+  mutate(value = round(planted / total * 100, 1)) %>% 
+  mutate(Country = "Latin America and the Caribbean") %>% 
+  select(Country, Years, value)
+
+# Add ECLAC to main data
+i2531 %<>% 
+  select(Country, Years, value) %>% 
+  bind_rows(eclac_totals) %>% 
+  arrange(Country, Years)
+
+# **********************************************
+
+join_keys <- setNames(dim_config2531$pub_col, dim_config2531$data_col)
+
+pub %<>% select(all_of(unname(join_keys)), value) # Keep only used labels
+
+comp <- full_join(i2531, pub, by = join_keys, suffix = c("", ".pub"))
+
+comp_sum <- get_comp_summary_table(comp, dim_config2531)
+
+### Run checks
+# (1) What dimensions were present in the old file but not in the new one?
+comp_sum %>% 
+  filter(status == "Old Only") #%>% View()
+# These are the manual edits to labels that are needed (or data loss that needs to be investigated)
+# This could also show summary rows that are in the data
+
+# (2) What dimensions are only present in the new file?
+comp_sum %>% 
+  filter(status == "New Only") #%>% View()
+# Expect to see the new year of data. Also check if any countries are new, and if so, why were they not included before? (Questions to ask Alberto)
+
+# (3) View all - this can help match up labels
+# comp_sum %>% filter(dim_name == "Type") %>% View()
+
+
+### filter only on labels in CEPALSTAT dims ***
+
+# **********************************************
+
+rm(eclac_totals)
+
+# ---- join CEPALSTAT dimension IDs ----
+
+# Join dimensions
+i2531f <- join_data_dim_members(i2531, dim_config2531)
+
+# Assert that there are no NA values
+assert_no_na_cols(i2531f)
+
+
+# ---- add metadata fields and export ----
+
+# manually update footnotes, if necessary
+get_indicator_footnotes(indicator_id)
+
+i2531f %<>% 
+  mutate(footnotes_id = "")
+
+# ***** overwrite footnotes**
+
+i2531f %<>% 
+  mutate(footnotes_id = ifelse(Country == "Latin America and the Caribbean", "6970", footnotes_id))
+
+# used to use footnote 7771 Includes Bonaire, Sint Eustatius and Saba -> for Ex Netherlands Antilles
+
+# ***************************
+
+i2531f %<>% 
+  select(ends_with("_id"), value)
+
+i2531f <- format_for_wasabi(i2531f, 2531)
+
+# Assert that there are no NA values
+assert_no_na_cols(i2531f)
+
+# Create a date/time stamp for export version control
+dt_stamp <- format(Sys.time(), "%Y-%m-%dT%H%M%S")
+
+# Export!
+# write_xlsx(i2531f, glue(here("Data/Cleaned/id{indicator_id}_{dt_stamp}.xlsx")))
+
+
+# ---- create comparison file ----
+
+# Begin with i2531 (before the switch to CEPALSTAT IDs) and pub
+# Rejoin comp (in case edits were made to data file)
+comp <- full_join(i2531, pub, by = join_keys, suffix = c("", ".pub"))
+
+# Join dimensions
+comp <- join_data_dim_members(comp, dim_config2531)
+
+# Assert that there are no NA values in non-value rows
+assert_no_na_cols(comp, !contains("value"))
+
+# Run comparison checks and format
+comp <- create_comparison_checks(comp, dim_config2531)
+
+comp
+
+# Export comp file!
+# write_xlsx(comp, here(glue("Data/Checks/comp_id{indicator_id}.xlsx")))
