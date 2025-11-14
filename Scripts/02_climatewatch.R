30b2d386b2220e5b311d4ff4e5e34d30e82da09d
Alexis Athens <lexisathens1@gmail.com>
2025-11-14 14:06:03 -0300

    Write indicator 2027 (combined indicator) code

--- Scripts/02_climatewatch.R
+++ Scripts/02_climatewatch.R
@@ -29,7 +29,7 @@ source(here("Scripts/process_indicator_fn.R"))
 iso <- read_xlsx(here("Data/iso_codes.xlsx"))
 
 iso %<>% 
-  filter(ECLACa == "Y") %>% 
+  filter(ECLACa == "Y" | name == "World") %>% 
   select(cepalstat, name, std_name)
 
 # ---- read downloaded files ----
@@ -143,4 +143,118 @@ result_3351 <- process_indicator(
   # source_fn = source_3351,
   diagnostics = TRUE,
   export = TRUE
+)
+
+
+## ---- indicator 2027 — carbon dioxide (CO₂) emissions (Total, per capita, and per GDP) ----
+
+indicator_id <- 2027 # co2 emissions
+
+# Fill out dim config table by matching the following info:
+# get_indicator_dimensions(indicator_id)
+# print(pub <- get_cepalstat_data(indicator_id) %>% match_cepalstat_labels())
+
+dim_config_2027 <- tibble(
+  data_col = c("Country", "Years", "Calculation"),
+  dim_id = c("208", "29117", "26653"),
+  pub_col = c("208_name", "29117_name", "26653_name")
+)
+
+filter_2027 <- function(data) {
+  data %>%
+    rename(
+      Country = country,
+      Years = year
+    ) %>%
+    mutate(
+      total_emissions = as.numeric(emissions),
+      population = as.numeric(population),
+      gdp_constant_2015_usd = as.numeric(gdp_constant_2015_usd)
+    ) %>%
+    select(
+      Country, Years, unit,
+      Total = total_emissions, Pop = population, GDP = gdp_constant_2015_usd
+    )
+}
+
+transform_2027 <- function(data) {
+  data
+}
+
+regional_2027 <- function(data) {
+  # create eclac total
+  eclac_totals <- data %>%
+    filter(Country != "World") %>% 
+    group_by(Years) %>%
+    summarise(Total = sum(Total, na.rm = TRUE),
+              Pop = sum(Pop, na.rm = TRUE),
+              GDP = sum(GDP, na.rm = TRUE), .groups = "drop") %>%
+    mutate(Country = "Latin America and the Caribbean")
+  
+  # manually subtract out venezuela with missing GDP data
+  missing_vz_gdp <- (nrow(data %>% filter(Country == "Venezuela" & is.na(GDP))) > 0)
+  
+  if(missing_vz_gdp) {
+    eclac_totals_gdp_adj <- data %>%
+      filter(Country != "World",
+             Country != "Venezuela") %>%          # exclude only here
+      group_by(Years) %>%
+      summarise(
+        Total_noVEN = sum(Total, na.rm = TRUE),      # emissions excluding VEN
+        GDP_noVEN = sum(GDP, na.rm = TRUE),       # GDP excluding VEN
+        .groups = "drop"
+      ) %>%
+      mutate(
+        Country = "Latin America and the Caribbean"
+      )
+    
+    eclac_totals %<>%
+      left_join(eclac_totals_gdp_adj, by = c("Years", "Country"))
+  }
+    
+  # join together
+  data <- bind_rows(data, eclac_totals) 
+  
+  # create calculation columns
+  data %<>% 
+    mutate(`Total, excluding land change use and forestry` = Total,
+           `Per capita` = Total / Pop * 1e6,
+           `By gross domestic product` = Total / GDP * 1e12) %>% 
+    select(-unit)
+  
+  if(missing_vz_gdp) {
+    data %<>% 
+      mutate(`By gross domestic product` = ifelse(Country == "Latin America and the Caribbean",
+                                                         Total_noVEN / GDP_noVEN * 1e12,
+                                                         `By gross domestic product`)) %>% 
+      select(-Total_noVEN, -GDP_noVEN)
+  }
+  
+  # clean df
+  data %<>% 
+    select(-Total, -Pop, -GDP) %>% 
+    pivot_longer(cols = c("Total, excluding land change use and forestry", "Per capita", "By gross domestic product"),
+                 names_to = "Calculation") %>% 
+    filter(!is.na(value)) # remove venezuela total per GDP data
+  
+  return(data)
+}
+
+footnotes_2027 <- function(data) {
+  data %>%
+    mutate(footnotes_id = ifelse(Country == "Latin America and the Caribbean", "6970", footnotes_id))
+}
+
+## Monday: export and review data
+
+result_2027 <- process_indicator(
+  indicator_id = indicator_id,
+  data = data_2027,
+  dim_config = dim_config_2027,
+  filter_fn = filter_2027,
+  transform_fn = transform_2027,
+  footnotes_fn = footnotes_2027,
+  regional_fn = regional_2027,
+  diagnostics = TRUE,
+  export = FALSE
 )
