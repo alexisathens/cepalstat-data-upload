93ebe2c5f89e942d0c814859951c1ede8c60327e
Alexis Athens <lexisathens1@gmail.com>
2026-07-23 17:41:09 -0400

    Update emdat script to be compatible with new process_indicator_fn

--- Scripts/0102_emdat.R
+++ Scripts/0102_emdat.R
@@ -1,16 +1,3 @@
-library(tidyverse)
-library(magrittr)
-library(readxl)
-library(httr2)
-library(jsonlite)
-library(glue)
-library(writexl)
-library(here)
-library(assertthat)
-library(quarto)
-library(CepalStatR)
-library(FAOSTAT)
-
 # This script cleans and standardizes the EM-DAT disaster indicators
 
 # ---- download ----
@@ -26,39 +13,32 @@ library(FAOSTAT)
 source(here("Scripts/utils.R"))
 source(here("Scripts/process_indicator_fn.R"))
 
-# Read in ISO with cepalstat ids
-iso <- read_xlsx(here("Data/iso_codes.xlsx"))
-
-iso %<>% 
-  filter(ECLACa == "Y") %>% 
-  select(cepalstat, name, std_name)
+# ---- data ----
 
 # Read in the downloaded emdat data
 emdat <- read_xlsx(here("Data/Raw/emdat/public_emdat_incl_hist_2026-07-06.xlsx"))
 
-# Define latest year of data
-max_year <- 2025
+# ---- shared functions ----
 
+# Define latest year of data
+max_year_emdat <- 2025
+
+# Standard data filter
+filter_emdat <- function(data) {
+  data %>% 
+    filter(`Disaster Subgroup` %in% c("Climatological", "Hydrological", "Meteorological", "Geophysical")) %>%
+    filter(as.numeric(`Start Year`) >= 1990 & as.numeric(`Start Year`) <= max_year) %>% 
+    filter(!Country %in% c("Sint Maarten (Dutch part)", "Bermuda"))
+}
 
 ## ---- indicator 4046 - economic cost of disasters ----
 indicator_id <- 4046
-
-# Fill out dim config table by matching the following info:
-# get_indicator_dimensions(indicator_id)
-# print(pub <- get_cepalstat_data(indicator_id) %>% match_cepalstat_labels())
 
 dim_config_4046 <- tibble(
   data_col = c("Country", "Years", "Type"),
   dim_id = c("208", "29117", "21714"),
   pub_col = c("208_name", "29117_name", "21714_name")
 )
-
-filter_4046 <- function(data) {
-  data %>% 
-    filter(`Disaster Subgroup` %in% c("Climatological", "Hydrological", "Meteorological", "Geophysical")) %>%
-    filter(as.numeric(`Start Year`) >= 1970 & as.numeric(`Start Year`) <= max_year) %>% 
-    filter(!Country %in% c("Sint Maarten (Dutch part)", "Bermuda"))
-}
 
 transform_4046 <- function(data) {
   data %<>% 
@@ -76,7 +56,6 @@ transform_4046 <- function(data) {
       `Disaster Subgroup` %in% c("Geophysical") ~ "Geophysical",
       TRUE ~ NA_character_)) %>%
     select(-`Disaster Subgroup`, -`Disaster Type`) %>% 
-    #filter(!is.na(value)) %>% 
     group_by(Country, Years, Group, Type) %>% 
     summarize(value = sum(value, na.rm = TRUE), .groups = "drop") %>% 
     mutate(value = value / 1000) # convert '000 USD to millions
@@ -91,120 +70,31 @@ transform_4046 <- function(data) {
     summarize(value = sum(value, na.rm = T), .groups = "drop") %>% 
     mutate(Type = "Total")
     
-  data %<>% 
+  data %>% 
     select(-Group) %>% 
     bind_rows(type_sum) %>% 
     bind_rows(total_sum)
 }
 
-footnotes_4046 <- function(data) {
-  data
-}
-
-result_4046 <- process_indicator(
+spec_4046 <- indicator_spec(
   indicator_id = 4046,
   data = emdat,
+  max_year = max_year_emdat,
   dim_config = dim_config_4046,
-  filter_fn = filter_4046,
-  transform_fn = transform_4046,
-  footnotes_fn = footnotes_4046,
-  diagnostics = TRUE,
-  export = TRUE
-)
-
-
-## ---- indicator 1837 - occurrence of disasters ----
-# this indicator is no longer maintained
-indicator_id <- 1837
-
-# Fill out dim config table by matching the following info:
-# get_indicator_dimensions(indicator_id)
-# print(pub <- get_cepalstat_data(indicator_id) %>% match_cepalstat_labels())
-
-dim_config_1837 <- tibble(
-  data_col = c("Country", "Years", "Type", "Indicator"),
-  dim_id = c("208", "29117", "21714", "21725"),
-  pub_col = c("208_name", "29117_name", "21714_name", "21725_name")
-)
-
-filter_1837 <- function(data) {
-  data %<>%
-    filter(`Disaster Subgroup` %in% c("Climatological", "Hydrological", "Meteorological", "Geophysical")) %>%
-    filter(as.numeric(`Start Year`) >= 1990 & as.numeric(`Start Year`) <= max_year) %>%
-    filter(!Country %in% c("Sint Maarten (Dutch part)", "Bermuda"))
-}
-
-transform_1837 <- function(data) {
-  data %<>%
-    select(`DisNo.`, `Disaster Subgroup`, `Disaster Type`, Country, Years = `Start Year`,
-           `Total Deaths`, `Total Affected`) %>%
-    mutate(Type = paste0(`Disaster Type`, "s")) %>%
-    mutate(Type = case_when(
-      Type == "Mass movement (wet)s" ~ "Wet mass displacement", # was "desplacement", fixed typo in CEPALSTAT admin
-      Type == "Volcanic activitys" ~ "Volcanic eruptions",
-      Type == "Mass movement (dry)s" ~ "Dry mass displacement",
-      TRUE ~ Type
-    )) %>%
-    mutate(Group = case_when(
-      `Disaster Subgroup` %in% c("Climatological", "Hydrological", "Meteorological") ~ "Climate change related",
-      `Disaster Subgroup` %in% c("Geophysical") ~ "Geophysical",
-      TRUE ~ NA_character_)) %>%
-    select(-`Disaster Subgroup`, -`Disaster Type`) %>%
-    mutate(`Number of events` = 1) %>%
-    select(-DisNo.) %>%
-    pivot_longer(cols = c(`Total Deaths`, `Total Affected`, `Number of events`), names_to = "Indicator") %>%
-    mutate(Indicator = case_when(
-      Indicator == "Total Deaths" ~ "Human deaths",
-      Indicator == "Total Affected" ~ "Directly affected persons",
-      TRUE ~ Indicator)) %>%
-    group_by(Country, Years, Group, Type, Indicator) %>%
-    summarize(value = sum(value, na.rm = T), .groups = "drop")
-
-  type_sum <- data %>%
-    group_by(Country, Years, Group, Indicator) %>%
-    summarize(value = sum(value, na.rm = T), .groups = "drop") %>%
-    rename(Type = Group)
-
-  data %<>%
-    select(-Group) %>%
-    bind_rows(type_sum)
-}
-
-footnotes_1837 <- function(data) {
-  data
-}
-
-result_1837 <- process_indicator(
-  indicator_id = 1837,
-  data = emdat,
-  dim_config = dim_config_1837,
-  filter_fn = filter_1837,
-  transform_fn = transform_1837,
-  footnotes_fn = footnotes_1837,
-  diagnostics = TRUE,
-  export = TRUE
+  filter_data = filter_emdat,
+  transform_data = transform_4046,
+  calculate_regional = calculate_regional_sum
 )
 
 
 ## ---- indicator 5647 - number of disasters ----
 indicator_id <- 5647
 
-# Fill out dim config table by matching the following info:
-# get_indicator_dimensions(indicator_id)
-# print(pub <- get_cepalstat_data(indicator_id) %>% match_cepalstat_labels())
-
 dim_config_5647 <- tibble(
   data_col = c("Country", "Years", "Type"),
   dim_id = c("208", "29117", "21714"),
   pub_col = c("208_name", "29117_name", "21714_name")
 )
-
-filter_5647 <- function(data) {
-  data %<>% 
-    filter(`Disaster Subgroup` %in% c("Climatological", "Hydrological", "Meteorological", "Geophysical")) %>%
-    filter(as.numeric(`Start Year`) >= 1990 & as.numeric(`Start Year`) <= max_year) %>% 
-    filter(!Country %in% c("Sint Maarten (Dutch part)", "Bermuda"))
-}
 
 transform_5647 <- function(data) {
   data %<>% 
@@ -232,50 +122,36 @@ transform_5647 <- function(data) {
     ungroup() %>% 
     rename(Type = Group)
   
-  data %<>% 
+  total_sum <- data %>% 
+    group_by(Country, Years) %>% 
+    summarize(value = sum(value, na.rm = T), .groups = "drop") %>% 
+    mutate(Type = "Total")
+  
+  data %>% 
     select(-Group) %>% 
-    bind_rows(type_sum)
+    bind_rows(type_sum) %>% 
+    bind_rows(total_sum)
 }
 
-footnotes_5647 <- function(data) {
-  data
-}
-
-source_5647 <- function(indicator_id) {
-  742  # Base de datos internacional de desastres (EM-DAT)
-}
-
-result_5647 <- process_indicator(
+spec_5647 <- indicator_spec(
   indicator_id = 5647,
   data = emdat,
+  max_year = max_year_emdat,
   dim_config = dim_config_5647,
-  filter_fn = filter_5647,
-  transform_fn = transform_5647,
-  footnotes_fn = footnotes_5647,
-  source_fn = source_5647,
-  diagnostics = TRUE,
-  export = TRUE
+  filter_data = filter_emdat,
+  transform_data = transform_5647,
+  calculate_regional = calculate_regional_sum
 )
+
 
 ## ---- indicator 5645 - deaths caused by disasters ----
 indicator_id <- 5645
-
-# Fill out dim config table by matching the following info:
-# get_indicator_dimensions(indicator_id)
-# print(pub <- get_cepalstat_data(indicator_id) %>% match_cepalstat_labels())
 
 dim_config_5645 <- tibble(
   data_col = c("Country", "Years", "Type"),
   dim_id = c("208", "29117", "21714"),
   pub_col = c("208_name", "29117_name", "21714_name")
 )
-
-filter_5645 <- function(data) {
-  data %<>% 
-    filter(`Disaster Subgroup` %in% c("Climatological", "Hydrological", "Meteorological", "Geophysical")) %>%
-    filter(as.numeric(`Start Year`) >= 1990 & as.numeric(`Start Year`) <= max_year) %>% 
-    filter(!Country %in% c("Sint Maarten (Dutch part)", "Bermuda"))
-}
 
 transform_5645 <- function(data) {
   data %<>% 
@@ -303,51 +179,36 @@ transform_5645 <- function(data) {
     ungroup() %>% 
     rename(Type = Group)
   
-  data %<>% 
+  total_sum <- data %>% 
+    group_by(Country, Years) %>% 
+    summarize(value = sum(value, na.rm = T), .groups = "drop") %>% 
+    mutate(Type = "Total")
+  
+  data %>% 
     select(-Group) %>% 
-    bind_rows(type_sum)
+    bind_rows(type_sum) %>% 
+    bind_rows(total_sum)
 }
 
-footnotes_5645 <- function(data) {
-  data
-}
-
-source_5645 <- function(indicator_id) {
-  742  # Base de datos internacional de desastres (EM-DAT)
-}
-
-result_5645 <- process_indicator(
+spec_5645 <- indicator_spec(
   indicator_id = 5645,
   data = emdat,
+  max_year = max_year_emdat,
   dim_config = dim_config_5645,
-  filter_fn = filter_5645,
-  transform_fn = transform_5645,
-  footnotes_fn = footnotes_5645,
-  source_fn = source_5645,
-  diagnostics = TRUE,
-  export = TRUE
+  filter_data = filter_emdat,
+  transform_data = transform_5645,
+  calculate_regional = calculate_regional_sum
 )
 
 
 ## ---- indicator 5646 - persons affected by disasters ----
 indicator_id <- 5646
 
-# Fill out dim config table by matching the following info:
-# get_indicator_dimensions(indicator_id)
-# print(pub <- get_cepalstat_data(indicator_id) %>% match_cepalstat_labels())
-
 dim_config_5646 <- tibble(
   data_col = c("Country", "Years", "Type"),
   dim_id = c("208", "29117", "21714"),
   pub_col = c("208_name", "29117_name", "21714_name")
 )
-
-filter_5646 <- function(data) {
-  data %<>% 
-    filter(`Disaster Subgroup` %in% c("Climatological", "Hydrological", "Meteorological", "Geophysical")) %>%
-    filter(as.numeric(`Start Year`) >= 1990 & as.numeric(`Start Year`) <= max_year) %>% 
-    filter(!Country %in% c("Sint Maarten (Dutch part)", "Bermuda"))
-}
 
 transform_5646 <- function(data) {
   data %<>% 
@@ -375,27 +236,76 @@ transform_5646 <- function(data) {
     ungroup() %>% 
     rename(Type = Group)
   
-  data %<>% 
+  total_sum <- data %>% 
+    group_by(Country, Years) %>% 
+    summarize(value = sum(value, na.rm = T), .groups = "drop") %>% 
+    mutate(Type = "Total")
+  
+  data %>% 
     select(-Group) %>% 
-    bind_rows(type_sum)
+    bind_rows(type_sum) %>% 
+    bind_rows(total_sum)
 }
 
-footnotes_5646 <- function(data) {
-  data # No footnote
-}
-
-source_5646 <- function(indicator_id) {
-  742  # Base de datos internacional de desastres (EM-DAT)
-}
-
-result_5646 <- process_indicator(
+spec_5646 <- indicator_spec(
   indicator_id = 5646,
   data = emdat,
+  max_year = max_year_emdat,
   dim_config = dim_config_5646,
-  filter_fn = filter_5646,
-  transform_fn = transform_5646,
-  footnotes_fn = footnotes_5646,
-  source_fn = source_5646,
-  diagnostics = TRUE,
-  export = TRUE
+  filter_data = filter_emdat,
+  transform_data = transform_5646,
+  calculate_regional = calculate_regional_sum
 )
+
+
+## ---- indicator 1837 - occurrence of disasters ----
+## this indicator is no longer maintained
+# indicator_id <- 1837
+# 
+# dim_config_1837 <- tibble(
+#   data_col = c("Country", "Years", "Type", "Indicator"),
+#   dim_id = c("208", "29117", "21714", "21725"),
+#   pub_col = c("208_name", "29117_name", "21714_name", "21725_name")
+# )
+# 
+# transform_1837 <- function(data) {
+#   data %<>%
+#     select(`DisNo.`, `Disaster Subgroup`, `Disaster Type`, Country, Years = `Start Year`,
+#            `Total Deaths`, `Total Affected`) %>%
+#     mutate(Type = paste0(`Disaster Type`, "s")) %>%
+#     mutate(Type = case_when(
+#       Type == "Mass movement (wet)s" ~ "Wet mass displacement", # was "desplacement", fixed typo in CEPALSTAT admin
+#       Type == "Volcanic activitys" ~ "Volcanic eruptions",
+#       Type == "Mass movement (dry)s" ~ "Dry mass displacement",
+#       TRUE ~ Type
+#     )) %>%
+#     mutate(Group = case_when(
+#       `Disaster Subgroup` %in% c("Climatological", "Hydrological", "Meteorological") ~ "Climate change related",
+#       `Disaster Subgroup` %in% c("Geophysical") ~ "Geophysical",
+#       TRUE ~ NA_character_)) %>%
+#     select(-`Disaster Subgroup`, -`Disaster Type`) %>%
+#     mutate(`Number of events` = 1) %>%
+#     select(-DisNo.) %>%
+#     pivot_longer(cols = c(`Total Deaths`, `Total Affected`, `Number of events`), names_to = "Indicator") %>%
+#     mutate(Indicator = case_when(
+#       Indicator == "Total Deaths" ~ "Human deaths",
+#       Indicator == "Total Affected" ~ "Directly affected persons",
+#       TRUE ~ Indicator)) %>%
+#     group_by(Country, Years, Group, Type, Indicator) %>%
+#     summarize(value = sum(value, na.rm = T), .groups = "drop")
+# 
+#   type_sum <- data %>%
+#     group_by(Country, Years, Group, Indicator) %>%
+#     summarize(value = sum(value, na.rm = T), .groups = "drop") %>%
+#     rename(Type = Group)
+# 
+#   total_sum <- data %>% 
+#     group_by(Country, Years) %>% 
+#     summarize(value = sum(value, na.rm = T), .groups = "drop") %>% 
+#     mutate(Type = "Total")
+#   
+#   data %>% 
+#     select(-Group) %>% 
+#     bind_rows(type_sum) %>% 
+#     bind_rows(total_sum)
+# }
