11b470556adfd2ae79da374e6b94d230b924ebb5
Alexis Athens <lexisathens1@gmail.com>
2026-07-24 09:52:17 -0400

    Base bulk upload on metadata sheet

--- Scripts/00_run_all.R
+++ Scripts/00_run_all.R
@@ -1,11 +1,14 @@
 source(here("Scripts/process_indicator_fn.R"))
 source(here("Scripts/0102_emdat.R"))
 
-# run
-
 # define global specs
 global_spec <- list(diagnostics = TRUE, export = FALSE, qc_check = FALSE, open_qmd = FALSE, metadata = FALSE)
 
+# define indicators to bulk run
+run_list <- meta %>% 
+  filter(source == "CRED") %>% 
+  pull(id)
 
-process_indicator(spec_4046, global_spec)
-# **why is this printing the data table at the end?
+run_list <- c("4046", "5647")
+
+result_4046 <- process_indicator(spec_4046, global_spec)
